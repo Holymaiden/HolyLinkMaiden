a5bfc8f15522c53ce5115035dc13d09c4d8e76d9
Holymaiden <43660765+Holymaiden@users.noreply.github.com>
2018-09-29 08:29:16 +0800

    Add files via upload

--- safelinkv2_utama.js
+++ safelinkv2_utama.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="09/4xp-Forex-Place-Review.html"}
-if(x=="2"){xxx="09/7-reasons-to-trade-forex.html"}
-if(x=="3"){xxx="09/anyoption-review-forex-brokers-reviews-ratings.html"}
-if(x=="4"){xxx="09/forex-social-platforms-dailyforex.html"}
-if(x=="5"){xxx="09/forex-swing-trading-strategies-DailyForex.html"}a_to_ve[i].href="https://holylinkmaiden.blogspot.com/2018/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="09/4xp-Forex-Place-Review.html#"}
+if(x=="2"){xxx="09/7-reasons-to-trade-forex.html#"}
+if(x=="3"){xxx="09/anyoption-review-forex-brokers-reviews-ratings.html#"}
+if(x=="4"){xxx="09/forex-social-platforms-dailyforex.html#"}
+if(x=="5"){xxx="09/forex-swing-trading-strategies-DailyForex.html#"}a_to_ve[i].href="https://holylinkmaiden.blogspot.com/2018/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
